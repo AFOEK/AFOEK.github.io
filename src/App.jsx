fb228fc0943e7c61046960e271e7eb8a29221d66
AFOEK <felix.montalfu@gmail.com>
2026-09-06 06:29:45 -0400

    ViteJS + React + Material UI

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button, Container, Typography } from "@mui/material";
+
+function App() {
+  return (
+    <Container>
+      <Typography variant="h2">Felix Montalfu</Typography>
+      <Typography variant="h5">Quantum Computing • AI • Computer Science</Typography>
+      <Button variant="contained">Research</Button>
+    </Container>
+  );
+}
+
+export default App;
